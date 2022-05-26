1f52684cda262c8ff633665f0dfd3a9d6ad438b1
shark <sharkshen@outlook.com>
2022-05-26 09:12:42 +0800

    1:Http 组件测试

---
+++ src/test/java/com/linkjb/camelcomponent/CamelComponentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkjb.camelcomponent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelComponentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
